f9464e5058010fa75c122cc35494a364c60c5aea
hyrki09 <117956417+hyrki09@users.noreply.github.com>
2025-06-23 07:43:19 +0900

    [level 3] Title: 이중우선순위큐, Time: 746.66 ms, Memory: 15.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,23 @@
+import heapq
+
+def solution(operations):
+    
+    h = []
+    
+    
+    for command in operations:
+        
+        command_line = command.split()
+        cm_str = command_line[0]
+        cm_num = command_line[1]
+        
+        if cm_str == "I":
+            heapq.heappush(h, int(cm_num))
+        else:
+            if cm_num == "1":
+                h=heapq.nlargest(len(h), h)[1:]
+                heapq.heapify(h)
+            elif cm_num == "-1" and len(h) > 0:
+                heapq.heappop(h)
+
+    return list(map(int, [heapq.nlargest(len(h), h)[0], h[0]])) if h else [0,0]
