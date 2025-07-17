77e9f3930a4833643587e4aa761a81780aca9f74
hyrki09 <117956417+hyrki09@users.noreply.github.com>
2025-07-17 22:10:54 +0900

    [level 2] Title: 3 x n 타일링, Time: 832.60 ms, Memory: 9.97 MB -BaekjoonHub

---
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = 0
+    
+    dp = [0] * (n+1)
+    
+    dp[0]=1
+    dp[2]=3
+    
+    for i in range(4,n+1,2):
+        dp[i] = 3*dp[i-2]
+        
+        for j in range(4,i+1,2):
+            dp[i] += 2*dp[i-j]
+            
+    
+    return dp[n]%1000000007
