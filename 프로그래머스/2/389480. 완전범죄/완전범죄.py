f46e441a0337ccd46bbfe75f6870c9adaa124af3
hyrki09 <117956417+hyrki09@users.noreply.github.com>
2025-07-21 11:12:42 +0900

    [level 2] Title: 완전범죄, Time: 13.64 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,8 +1,7 @@
 def solution(info, n, m):
-    answer = 0
-    dp = [[False] *m for _ in range(n)]
+
+    dp = [[False] * m for _ in range(n)]
     dp[0][0] = True
-    
     
     for ai,bi in info:
         next_dp = [[False] * m for _ in range(n)]
@@ -10,21 +9,19 @@ def solution(info, n, m):
             for b in range(m):
                 if not dp[a][b]:
                     continue
-                na = ai + a
+                
+                na = a + ai
                 if na < n:
-                    next_dp[na][b]=True
-                
-                nb = bi + b
+                    next_dp[na][b] = True
+                nb = b + bi
                 if nb < m:
-                    next_dp[a][nb]=True
-                    
-        dp = next_dp    
+                    next_dp[a][nb] = True
                 
-    
+        dp = next_dp
+        
+        
     for a in range(n):
         for b in range(m):
             if dp[a][b]:
                 return a
-
-    
     return -1
